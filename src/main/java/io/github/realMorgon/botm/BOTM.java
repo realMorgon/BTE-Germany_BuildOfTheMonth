f1e0370a654f842bb2b11cc8c7d3b3dd46adc811
Morgon <moritz.olthoff@gmail.com>
2025-01-16 19:09:24 +0100

    Initial commit

---
+++ src/main/java/io/github/realMorgon/botm/BOTM.java
@@ -0,0 +1,17 @@
+package io.github.realMorgon.botm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BOTM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/io/github/realMorgon/botm/SaveConfig.java
@@ -0,0 +1,4 @@
+package io.github.realMorgon.botm;
+
+public class SaveConfig {
+}
